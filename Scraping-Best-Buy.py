8351c21f6de1820c149641bc84718c593d1fb5e5
Frane Kozemelj <81092595+fkozemelj@users.noreply.github.com>
2021-03-21 13:41:12 -0700

    Update Scraping-Best-Buy.py

--- Scraping-Best-Buy.py
+++ Scraping-Best-Buy.py
@@ -2,8 +2,10 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
+#Location of chromedriver on your local machine
 driver = webdriver.Chrome('C:\Program Files\Google\Chrome\Application\chromedriver.exe')
 
+#Location of the data you want to scrape
 driver.get("https://www.bestbuy.ca/en-ca/search?search=microsoft+surface+3")
 
 products=[] #List to store name of the product
@@ -17,11 +19,11 @@ for a in soup.findAll(attrs={'class':'col-xs-12_198le col-sm-4_13E9O col-lg-3_EC
     name=a.find('div', attrs={'class':'productItemName_3IZ3c'})
     price=a.find('div', attrs={'class':'productPricingContainer_3gTS3'})
     rating=a.find('div', attrs={'class':'ratingContainer_29ZF-'})
-    #shipping=a.find('div',attrs={'class': 'availabilityMessageSearch_23ZLw'})
+    shipping=a.find('div',attrs={'class': 'availabilityMessageSearch_23ZLw'})
     products.append(name.text)
     prices.append(price.text)
     ratings.append(rating.text)
-    #availability.append(shipping.text)
+    availability.append(shipping.text)
 
 i = 0
 while i < len(prices):
